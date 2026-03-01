5106beda92abc28218b1989dc5409ec1c71b4918
Alexandre Miguel Prior Dias Das Neves <alemigue@c2r4s7.42porto.com>
2026-03-01 17:28:31 +0000

    update

--- Push_Swap/srcs/stack_init.c
+++ Push_Swap/srcs/stack_init.c
@@ -6,7 +6,7 @@
 /*   By: alemigue <alemigue@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/27 17:00:30 by alemigue          #+#    #+#             */
-/*   Updated: 2026/03/01 12:32:32 by alemigue         ###   ########.fr       */
+/*   Updated: 2026/03/01 17:28:00 by alemigue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,21 @@ Stack *stack_new (void)
 	if (!stack)
 		return(NULL);
 	stack->top = NULL;
+}
+
+void stack_push(Stack *stack, int value)
+{
+	Node	*new;
+
+	new = malloc(sizeof(Node));
+	if (!new)
+		return ;
+	new -> value = value;
+	new -> index = -1;
+	new -> next = stack -> top;
+	stack -> size ++;
+}
+int stack_pop(Stack	*stack)
+{
 
 }
