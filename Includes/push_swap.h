636d0557ad081a0a98340d8b044cef24050dd37f
Alexandre Miguel Prior Dias Das Neves <alemigue@c2r4s7.42porto.com>
2026-03-01 12:33:08 +0000

    Push_Swap the beggining

---
+++ Includes/push_swap.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alemigue <alemigue@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/27 16:59:29 by alemigue          #+#    #+#             */
+/*   Updated: 2026/03/01 12:32:22 by alemigue         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <stdlib.h>
+#include <stdlib.h> 
+#include <stdlib.h>
+
+// ─── Structures ───────────────────────────────────────────
+
+typedef struct Node
+{
+	int	value;
+	int	index;
+	struct	Node*  next;
+}Node;
+
+typedef struct Stack
+{
+	Node	*top;
+	int	size;
+}	Stack;
+
+// ─── Stack Init ───────────────────────────────────────────
