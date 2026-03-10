3d2c0c0aeb0594b81cec2eee09dc6c3c3da5b49a
PriorLab <nevesalexandre10@hotmail.com>
2026-03-10 14:07:20 +0000

    Update parsing.c

--- Push_Swap/srcs/parsing.c
+++ Push_Swap/srcs/parsing.c
@@ -17,10 +17,12 @@ int	ft_is_number(char *str)
 	int	i;
 
 	i = 0;
-	if (!str)
+	if (!str || !str[0])
 		return (0);
 	if (str[i] == '-' || str[i] == '+')
 		i++;
+	if (!str[i])
+		return (0);
 	while (str[i])
 	{
 		if (str[i] < '0' || str[i] > '9')
