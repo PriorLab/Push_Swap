4963dbac28d850a2c893bd11a6b3e0f1e2dd6ae3
PriorLab <nevesalexandre10@hotmail.com>
2026-03-20 14:02:53 +0000

    Update sort_small.c

--- Push_Swap/srcs/algorithms/sort_small.c
+++ Push_Swap/srcs/algorithms/sort_small.c
@@ -12,6 +12,7 @@
 
 #include "../../includes/push_swap.h"
 
+
 static void	sort_two(t_stack *a)
 {
 	if (a->top->value > a->top->next->value)
@@ -27,36 +28,64 @@ static void	sort_three(t_stack *a)
 	top = a->top->value;
 	mid = a->top->next->value;
 	bot = a->top->next->next->value;
+	
 	if (top > mid && mid < bot && top < bot)
 		sa(a);
-	if (top > mid && mid < bot && top > bot)
+	else if (top > mid && mid < bot && top > bot)
 		ra(a);
-	if (top < mid && mid > bot && top > bot)
+	else if (top < mid && mid > bot && top > bot)
 		rra(a);
-	if (top > mid && mid > bot && top > bot)
+	else if (top > mid && mid > bot && top > bot)
 	{
 		sa(a);
 		rra(a);
 	}
-	if (top < mid && mid > bot && top < bot)
+	else if (top < mid && mid > bot && top < bot)
 	{
 		rra(a);
 		sa(a);
 	}
 }
 
+static void	sort_four(t_stack *a, t_stack *b)
+{
+	// Move o menor (index 0) para b
+	while (a->size > 3)
+	{
+		if (a->top->index == 0)
+			pb(a, b);
+		else
+			ra(a);
+	}
+	
+	// Ordena os 3 que ficaram em a
+	sort_three(a);
+	
+	// Volta o menor para a
+	pa(a, b);
+}
+
 static void	sort_five(t_stack *a, t_stack *b)
 {
-	while (a -> size > 3)
+	// Move os 2 menores (index 0 e 1) para b
+	while (a->size > 3)
 	{
 		if (a->top->index == 0 || a->top->index == 1)
 			pb(a, b);
 		else
 			ra(a);
 	}
+	
+	// Ordena os 3 que ficaram em a
 	sort_three(a);
-	sort_two(b);
-	while (b->top)
+	
+	// Garante que o menor (index 0) está em cima de b
+	if (b->size == 2 && b->top->index == 1)
+		sb(b);
+	
+	// Volta os 2 menores para a (já ordenados)
+	pa(a, b);
+	if (b->top)
 		pa(a, b);
 }
 
@@ -66,6 +95,8 @@ void	sort_small(t_stack *a, t_stack *b)
 		sort_two(a);
 	else if (a->size == 3)
 		sort_three(a);
-	else
+	else if (a->size == 4)
+		sort_four(a, b);
+	else if (a->size == 5)
 		sort_five(a, b);
 }
